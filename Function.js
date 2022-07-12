955c0409326bd1eff07bd46fe576861f51ee61df
changliu08 <8854960+changliu08@users.noreply.github.com>
2022-07-12 12:16:22 +0800

    Update Function.js

--- Function.js
+++ Function.js
@@ -6,7 +6,21 @@ Function.prototype.myBind = function(context){
         if (this instanceof inner) {
             return new func(...outArgs, ...args);
         } else {
-            return func.call(context, outArgs.concat(args));
+            return func.apply(context, outArgs.concat(args));
+        }
+    }
+}
+
+function curry(fn){
+    var finalArgs = [];
+    return function inner(){
+        var args = Array.prototype.slice.call(arguments);
+        if (args.length < fn.length) {
+            var start = finalArgs.length > 0 ? finalArgs.length - 1: 0;
+            finalArgs.splice(start, 0, ...args);
+            return inner; 
+        } else {
+            return fn.apply(null, finalArgs);
         }
     }
 }
